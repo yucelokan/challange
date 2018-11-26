bb68e9b794b9ab8331f2733e90b8e1bfef1b159d
Okan Yücel <yucel.okan@outlook.com>
2018-11-26 13:53:24 +0300

    Initial Commit

---
+++ Challange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challange
+//
+//  Created by Okan Yücel on 26.11.2018.
+//  Copyright © 2018 Okan Yücel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
